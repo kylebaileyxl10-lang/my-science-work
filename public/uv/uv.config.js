41615b1aba7a674d44c2620f7cc61c4dbab24d66
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-05-14 05:37:52 -0400

    update

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,13 +1,11 @@
-/* Updated uv.config.js for v3.2.10 */
 self.__uv$config = {
-    prefix: "/uv/service/",
-    bare: "/bare/", 
-    wisp: "wss://wisp.mercurynetwork.org/wisp/", 
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    client: "/uv/uv.client.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
+  prefix: "/uv/service/",
+  wisp: "wss://wisp.mercurynetwork.org/wisp/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
 };
