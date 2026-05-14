e1c5b93802a9159b690292299c7cef4a700fcd9c
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-05-13 21:00:30 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,14 @@
-// This file overwrites the stock UV config.js
+/* This file overwrites the stock UV config.js */
 
 self.__uv$config = {
-	prefix: "/uv/service/",
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/uv/uv.handler.js",
-	client: "/uv/uv.client.js",
-	bundle: "/uv/uv.bundle.js",
-	config: "/uv/uv.config.js",
-	sw: "/uv/uv.sw.js",
+    prefix: "/uv/service/",
+    bare: "/bare/", 
+    wisp: "wss://wisp.mercurynetwork.org/", // This connects you to a working proxy server
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: "/uv/uv.handler.js",
+    client: "/uv/uv.client.js",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
+    sw: "/uv/uv.sw.js",
 };
