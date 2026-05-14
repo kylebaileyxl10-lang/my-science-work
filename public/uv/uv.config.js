e0e3288435ea89fa64428b9d5c4970642955ad74
kylebaileyxl10-lang <kylebaileyxl10@gmail.com>
2026-05-13 21:05:01 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,9 +1,8 @@
-/* This file overwrites the stock UV config.js */
-
+/* Updated uv.config.js for v3.2.10 */
 self.__uv$config = {
     prefix: "/uv/service/",
     bare: "/bare/", 
-    wisp: "wss://wisp.mercurynetwork.org/", 
+    wisp: "wss://ruby.rubynetwork.co/wisp/", 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: "/uv/uv.handler.js",
